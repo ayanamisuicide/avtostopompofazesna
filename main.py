e93c43b225685c1d85e2bb0c3ad7f35b01eb910e
ashley <136754527+ayanamisuicide@users.noreply.github.com>
2026-06-06 21:19:12 +0300

    проверяем существует ли папка downloads.

--- main.py
+++ main.py
@@ -4,3 +4,8 @@ downloads_folder = Path.home() / "Downloads"
 
 print("Организатор файлов запущен.")
 print(f"Папка для сортировки: {downloads_folder}")
+
+if downloads_folder.exists():
+    print("Папка существует.")
+else:
+    print("Папка не найдена.")
