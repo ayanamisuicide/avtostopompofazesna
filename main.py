ba9eadd34c6fb105019453de7874993a21d25e48
ashley <136754527+ayanamisuicide@users.noreply.github.com>
2026-06-06 21:23:46 +0300

    посмотрим содержимое папки Downloads, ничего не перемещая.

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ print(f"Папка для сортировки: {downloads_folder}")
 
 if downloads_folder.exists():
     print("Папка существует.")
+    for item in downloads_folder.iterdir():
+        print(item.name)
 else:
     print("Папка не найдена.")
 for item in downloads_folder.iterdir():
